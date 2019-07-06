697f5a63d1f7831b37c9787f3917e71550739722
ArthurCamloet <32558900+ArthurCamloet@users.noreply.github.com>
2019-07-06 15:29:20 +0530

    Add files via upload

---
+++ MyWatchList/functions/session.php
@@ -0,0 +1,18 @@
+<?php
+  //creates a session for storing data across pages
+  session_start();
+
+  if($_SESSION['logged_in'] != true){
+    header("location: ../login.php");
+  }
+  else {
+    $first_name = $_SESSION['first_name'];
+    $middle_name = $_SESSION['middle_name'];
+    $last_name = $_SESSION['last_name'];
+    $admin_tag = $_SESSION['admin_tag'];
+    $user_id = $_SESSION['user_id'];
+    $message = $_SESSION['message'];
+    $status = $_SESSION['status'];
+
+  }
+?>
